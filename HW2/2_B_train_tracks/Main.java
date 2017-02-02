b2dbbecbf8e11f92c96c410ea5c11d037ba854b6
Mark Surnin <marksurnin@gmail.com>
2017-02-02 15:12:36 -0500

    Add solution to 2_B_train_tracks.

---
+++ HW2/2_B_train_tracks/Main.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // set up I/O
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line;
+        int m = 0;
+        int f = 0;
+
+        try {
+            line = br.readLine();
+            int numTestCases = Integer.parseInt(line);
+
+            // advance to the url data
+            for (int i = 1; i <= numTestCases; i++) {
+                line = br.readLine();
+
+                for (char ch : line.toCharArray()) {
+                    if (ch == 'M') m++;
+                    if (ch == 'F') f++;
+                }
+
+                if (m == f && m > 1) {
+                    System.out.println("LOOP");
+                } else {
+                    System.out.println("NO LOOP");
+                }
+
+                // reset m & f counters
+                m = 0;
+                f = 0;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
