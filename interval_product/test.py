e341d8269861d362e969a83c013e75e8e24f2a3a
Mark Surnin <marksurnin@gmail.com>
2017-04-16 16:55:01 -0400

    Remove all .class files.

--- interval_product/test.py
+++
@@ -1,7 +0,0 @@
-from math import tanh
-
-xs = [-3, -2.5, -2, -1.5, -1, -0.5, -0.25, 0, 0.25, 0.5, 1, 1.5, 2, 2.5, 3]
-
-for x in xs:
-    output = tanh(1.5*x) - tanh(0.75*x)
-    print(x, ": ", output)
